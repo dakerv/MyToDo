2c0a48294e29dd22cf816ee568134203fff3f1f1
dakerv <vedaker@st.ug.edu.gh>
2025-11-17 14:56:23 +0000

    Imported Sidebar to Reminders page

--- app/Reminders.tsx
+++ app/Reminders.tsx
@@ -1,14 +1,30 @@
-import { Link } from 'expo-router';
-import React from "react";
+
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import Sidebar, { Pages } from '@/components/sidebar';
 
 
 export default function Reminders () {
+
+    const [isSideBarOpen, setIsSidebarOpen] = useState(false);
+    const [activePage, setActivePage] = useState<Pages>('Reminders')
+
+    const handleActivePage = (page: 'Tasks' | 'Reminders' | 'Themes') => {
+        setActivePage(page);
+        setIsSidebarOpen(false);
+    }
+
     return (
         <View style = {styles.container}>
             <Text> Hello, Welcome  </Text> 
-            <Link href={'/Themes'} style={styles.button}>
-            Go to the Themes page. </Link>
+            
+
+
+        <Sidebar 
+            isOpen={isSideBarOpen} 
+            onSelectPage={handleActivePage} 
+            selectedPage={activePage}
+            />
         </View>
     )
 }
