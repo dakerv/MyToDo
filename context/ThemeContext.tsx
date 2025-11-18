2a679b8046a17faf854cac5bad090fd7a672179a
dakerv <vedaker@st.ug.edu.gh>
2025-11-18 14:55:53 +0000

    Set up generic styles for the themes

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,1 +1,31 @@
 import { useContext, useState, createContext } from "react";
+
+type ThemeName = 'Petal' | 'Mint' | 'Neutral';
+
+type Theme = {
+    backgroundColor: string;
+    card: string;
+    text: string;
+    accent: string;
+}
+
+const themes : Record <ThemeName, Theme> = {
+    Petal: {
+        backgroundColor: 'pink',
+        card: 'lightpink',
+        text: 'darkred',
+        accent: 'deeppink',
+    },
+    Mint: {
+        backgroundColor: 'lightgreen',
+        card: 'palegreen',
+        text: 'darkgreen',
+        accent: 'mediumseagreen',
+    },
+    Neutral: {
+        backgroundColor: 'lightgray',
+        card: 'gainsboro',
+        text: 'dimgray',
+        accent: 'gray',
+    }
+}
